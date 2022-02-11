04ddb86afab0e64aa9b834ee3c41dfc87f291a7c
hchipma <99456451+hchipma@users.noreply.github.com>
2022-02-10 20:03:42 -0600

    Add files via upload

---
+++ newdle.py
@@ -0,0 +1,79 @@
+import os
+import psycopg2
+import psycopg2.extras
+from flask import Flask, request, render_template, g
+
+# PostgreSQL IP address
+IP_ADDR = "127.0.0.1"
+
+# Create the application
+app = Flask(__name__)
+
+#####################################################
+# Database handling 
+  
+def connect_db():
+    """Connects to the database."""
+    debug("Connecting to DB.")
+    #conn = psycopg2.connect(host=IP_ADDR, user="postgres", password="rhodes", dbname="newdle", 
+     #   cursor_factory=psycopg2.extras.DictCursor)
+    conn = psycopg2.connect("dbname=newdle user=postgres password=rhodes")
+    return conn
+    
+def get_db():
+    """Opens a new database connection if there is none yet for the
+    current application context.
+    """
+    if not hasattr(g, 'pg_db'):
+        g.pg_db = connect_db()
+    return g.pg_db
+    
+@app.teardown_appcontext
+def close_db(error):
+    """Closes the database automatically when the application
+    context ends."""
+    debug("Disconnecting from DB.")
+    if hasattr(g, 'pg_db'):
+        g.pg_db.close()
+
+######################################################
+# Command line utilities 
+        
+def init_db():
+    db = get_db()
+    with app.open_resource('schema.sql', mode='r') as f:
+        db.cursor().execute(f.read())
+    db.commit()
+
+@app.cli.command('initdb')
+def init_db_command():
+    """Initializes the database."""
+    print("Initializing DB.")
+    init_db()
+    
+    
+#####################################################
+# Debugging
+
+def debug(s):
+    """Prints a message to the screen (not web browser) 
+    if FLASK_DEBUG is set."""
+    if app.config['DEBUG']:
+        print(s)
+
+@app.route('/')
+def login():
+  return render_template("newdle.html")
+
+@app.route('/home')
+def home():
+  return render_template("newdle2.html")
+
+@app.route('/day',methods=['get', 'post'])
+def day():
+    db = get_db()
+    cursor = db.cursor()
+    cursor.execute("""select name from Students where name='Hannah'""")
+    rowlist = cursor.fetchall()
+    return render_template("newdle3.html",studentinfo=rowlist)
+
